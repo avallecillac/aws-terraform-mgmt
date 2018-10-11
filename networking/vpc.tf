9646acf47233060b43e08fcbd40b47b9d3c376e7
Alejandro Vallecilla <avallecillac@gmail.com>
2018-10-11 16:07:50 -0500

    AWS VPC and networking scenario 2
    AWS VPC and networking scenario 2 implementation with terraform

--- networking/vpc.tf
+++ networking/vpc.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+    region = "${var.region}"
+    profile = "${var.profile}"
+}
+
 resource "aws_vpc" "default_vpc" {
   cidr_block = "10.0.0.0/16"
 }
@@ -5,36 +10,38 @@ resource "aws_vpc" "default_vpc" {
 resource "aws_subnet" "private_subnet" {
   vpc_id = "${aws_vpc.default_vpc.id}"
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = "${aws_vpc.default_vpc.id}"
   cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
 }
 
-resource "aws_internet_gateway_" "vpc_igw" {
+resource "aws_internet_gateway" "vpc_igw" {
   vpc_id = "${aws_vpc.default_vpc.id}"
 }
 
 resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.default_vpc.id}"
+
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.vpc_igw.id}"
+  }
 }
 
-resource "aws_route" "igw_route" {
-  route_table_id = "${aws_route_table.public_route_table.id}"
-  destination_cidr_block = "0.0.0.0./0"
-  gateway_id = "${aws_internet_gateway_.vpc_igw.id}"
-}
-
-resource "aws_route_table_association" "rt_association" {
+resource "aws_route_table_association" "rt_public_association" {
   subnet_id = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route.igw_route.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
 }
 
 resource "aws_eip" "elastic_ip" {
-  vpc = "${aws_vpc.default_vpc.id}"
-  depends_on = "${aws_internet_gateway_.vpc_igw}"
+  vpc = true
+
+  depends_on = ["aws_internet_gateway.vpc_igw"]
 }
 
 resource "aws_nat_gateway" "nat_gw" {
@@ -42,17 +49,15 @@ resource "aws_nat_gateway" "nat_gw" {
   allocation_id = "${aws_eip.elastic_ip.id}"
 }
 resource "aws_route_table" "private_route_table" {
-  vpc_id = "${aws_vpc.default_vpc}"
-}
+  vpc_id = "${aws_vpc.default_vpc.id}"
 
-resource "aws_route" "nat_route" {
-  route_table_id = "${aws_route_table.private_route_table}"
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = "${aws_nat_gateway.nat_gw.id}"
+  route {
+      cidr_block = "0.0.0.0/0"
+      nat_gateway_id = "${aws_nat_gateway.nat_gw.id}"
+  }
 }
 
 resource "aws_route_table_association" "rt_nat_association" {
-  subnet_id = "${aws_route_table.private_route_table}"
-  route_table_id = "${aws_route.nat_route.id}"
+  subnet_id = "${aws_subnet.private_subnet.id}"
+  route_table_id = "${aws_route_table.private_route_table.id}"
 }
-
